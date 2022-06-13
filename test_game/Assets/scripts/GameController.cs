218aba7633422f2d110ca4053a795c96960402da
erlikart <81412552+erlikart@users.noreply.github.com>
2022-06-14 00:39:16 +0300

    v0.3

--- test_game/Assets/scripts/GameController.cs
+++ test_game/Assets/scripts/GameController.cs
@@ -10,34 +10,40 @@ public class GameController : MonoBehaviour
     [SerializeField] GameObject GridParent;
     public ReadInput myScript;
     public int GridSize = 3;
-    
+
     private void Start()
     {
         GridSize = ReadInput.intInput;
         Debug.Log(GridSize);
         GridParent.transform.GetComponent<GridLayoutGroup>().constraintCount = GridSize;
+        float newCellSize = 0;
+        if (GridSize <= 9)
+        {
+            newCellSize = 110;
+            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(newCellSize, newCellSize);
 
+
+        }
+        else if (GridSize >= 10 && GridSize <= 30)
+        {
+            newCellSize = 900 / GridSize;
+            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(newCellSize, newCellSize);
+        }
+        else
+        {
+            newCellSize = 700 / GridSize;
+            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(newCellSize, newCellSize);
+        }
+
+        for (int i = 0; i < CubePrefab.transform.childCount; i++)
+        {
+            CubePrefab.transform.GetChild(i).GetComponent<RectTransform>().sizeDelta = new Vector2(newCellSize, newCellSize);
+        }
         for (int i = 0; i < GridSize * GridSize; i++)
         {
             var obj = Instantiate(CubePrefab, GridParent.transform);
             obj.SetActive(true);
-            
-        }
 
-       if (GridSize <= 9) 
-        {
-            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(110, 110);
-            
-            
-        }else if ( GridSize >= 10 && GridSize <= 30 )
-        {
-            float newCellSize = 900 / GridSize;
-            
-            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(newCellSize, newCellSize);
-        }else
-        {
-            float newCellSize = 700 / GridSize;
-            GridParent.transform.GetComponent<GridLayoutGroup>().cellSize = new Vector2(newCellSize, newCellSize);
         }
     }
 }
